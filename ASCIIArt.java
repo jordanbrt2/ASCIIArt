b75d1d62683afad4b3ae2c31ca7efd441d62544d
Jordan Barrett <46046016+jordanbrt2@users.noreply.github.com>
2019-08-18 21:01:23 +0100

    inital commit

---
+++ ASCIIArt.java
@@ -0,0 +1,103 @@
+
+public class ASCIIArt {
+
+	public static void main(String[] args) throws InterruptedException {
+        // A loop that runs endless 
+		while (true) {
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+
+
+			System.out.println("         --------------");
+
+			System.out.println("           ||           ");
+
+			System.out.println("       /--------       ");
+
+			System.out.println(" LOL====      ()\\     ");
+
+			System.out.println("  L    \\         \\        ");
+
+			System.out.println("        \\_________]                ");
+
+			System.out.println("           I     I      ");
+
+			System.out.println("        -----------/              ");
+
+			System.out.println("                       ");
+
+			System.out.println("      Jordan's ROFLCOPTER!              ");
+
+
+			// A method that can pause the execution of current thread for certain time in milliseconds 
+			Thread.sleep(100);
+
+
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+			System.out.println();
+
+
+
+			System.out.println("--------------         ");
+
+			System.out.println("           ||           ");
+
+			System.out.println("  L    /--------       ");
+
+			System.out.println(" LOL====      ()\\     ");
+
+			System.out.println("       \\         \\        ");
+
+			System.out.println("        \\_________]                ");
+
+			System.out.println("           I     I      ");
+
+			System.out.println("        -----------/              ");
+
+			System.out.println("                       ");
+
+			System.out.println("      Jordan's ROFLCOPTER!              ");
+			
+
+
+			// A method that can pause the execution of current thread for certain time in milliseconds 
+			Thread.sleep(100);
+
+		}
+
+
+
+	}
+
+
+
+}
